5346134028cb3fef3760a7cb85c91f3556dec9ef
Dhatri-nanda <80507013+Dhatri-nanda@users.noreply.github.com>
2021-09-17 09:44:06 +0530

    Update code.py

--- Assignment_1/code.py
+++ Assignment_1/code.py
@@ -48,3 +48,23 @@ plt.plot(O[0],O[1],'o')
 plt.text(O[0] * (1 - 0.1), O[1] * (1 + 0.05) , 'O')
 plt.legend()
 plt.show()
+
+import soundfile as sf
+from scipy import signal
+#read .wav file
+input_signal,fs = sf.read('Sound_Noise.wav')
+#sampling frequency of Input signal
+sampl_freq=fs
+#order of the filter
+order=4
+#cutoff frquency 4kHz
+cutoff_freq=4000.0
+#digital frequency
+Wn = 2*cutoff_freq/sampl_freq
+# b and a are numerator and denominator polynomials respectively
+b, a = signal.butter(order,Wn, 'low')
+#filter the input signal with butterworth filter
+output_signal = signal.filtfilt(b, a, input_signal)
+#output signal = signal.lfilter(b, a, input_signal)
+#write the output signal into .wav file
+sf.write('Sound_With_ReducedNoise.wav', output_signal, fs)
